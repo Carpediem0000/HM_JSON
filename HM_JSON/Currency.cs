d7d86b0de2c73f0f47cf7d7377932d9b4fe7c16e
CarpeDiem0000 <zinovchukdfuz2@gmail.com>
2024-04-17 12:16:48 +0300

    Add project files.

---
+++ HM_JSON/Currency.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_JSON
+{
+    public class Currency
+    {
+        static int COUNTER = 1;
+        public int ID;
+        public int r030 { get; set; }
+        public string txt { get; set; }
+        public float rate { get; set; }
+        public string cc { get; set; }
+        public string exchangedate { get; set; }
+
+        public Currency(int r030, string txt, float rate, string cc, string exchangedate)
+        {
+            ID = COUNTER++;
+            this.r030 = r030;
+            this.txt = txt;
+            this.rate = rate;
+            this.cc = cc;
+            this.exchangedate = exchangedate;
+        }
+
+        public static void Show(List<Currency> currencyList)
+        {
+            int leftMargin = 0;
+            int topMargin = 0;
+
+            foreach (var item in currencyList)
+            {
+                Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop);
+                Console.Write($"{item.ID}. UAH -> {item.cc}({item.txt})");
+                Console.SetCursorPosition(45, Console.CursorTop);
+                Console.WriteLine($"{item.rate.ToString("F2")} UAH = 1 {item.cc}");
+                topMargin++;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{ID}. UAH -> {cc}({txt})   {rate}";
+        }
+    }
+}
---
+++ HM_JSON/Program.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft;
+using Newtonsoft.Json;
+using System.Net;
+namespace HM_JSON
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<Currency> currencyList = new();
+            try 
+            {
+                currencyList = await GetCurrencyList();
+
+            } 
+            catch (Exception e)
+            {
+                await Console.Out.WriteLineAsync(e.Message);
+            }
+
+            int choice = 0;
+            float sum;
+            string res;
+            do
+            {
+                Currency.Show(currencyList);
+                await Console.Out.WriteLineAsync("Чтобы закрыть программу введите 0");
+                await Console.Out.WriteLineAsync("Введите номер пары:");
+                choice = int.Parse(Console.ReadLine());
+
+                Currency currency = currencyList.FirstOrDefault(c => c.ID == choice);
+                if (currency != null)
+                {
+                    await Console.Out.WriteLineAsync("Введите сумму в грн:");
+                    sum = float.Parse(Console.ReadLine());
+                    res = $"{sum} UAH = {(sum / currency.rate).ToString("F2")} {currency.cc}({currency.txt})";
+                    await Console.Out.WriteLineAsync(res);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    await Console.Out.WriteLineAsync("Неправильный выбор");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            } while (choice != 0);
+        }
+        static async Task<List<Currency>> GetCurrencyList()
+        {
+            var currencyList = new List<Currency>();
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                currencyList = JsonConvert.DeserializeObject<List<Currency>>(responseContent);
+
+                return currencyList;
+            }
+            else
+            {
+                await Console.Out.WriteLineAsync("Не удалось получить информацию о валюте.");
+                return new List<Currency>();
+            }
+        }
+    }
+}
